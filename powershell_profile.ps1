aa47f8e540522b029c0b14ad2d120a83ca57563b
Kristo Copani <kristocopani@gmail.com>
2024-12-16 20:55:34 +0200

    asdf

---
+++ powershell_profile.ps1
@@ -0,0 +1,79 @@
+#Functions
+Function Ex { 
+    Exit 
+}
+
+Function Get-Ips {
+    $Publicip = (Curl Icanhazip.Com).Content
+    $Localip = (Get-Netipaddress | Where-Object { $_.Addressstate -Eq "Preferred" -And $_.Validlifetime -Lt "24:00:00" }).Ipaddress
+    Write-Host "Public Ip: $Publicip `Rlocal Ip: $Localip"
+}
+
+Function Reload {
+    . $Profile
+}
+
+Function Update-Dotfiles {
+    $Dotfile = (Curl Https://Raw.Githubusercontent.Com/Kristocopani/Dotfiles/Main/Windows%20terminal/Install.Ps1).Content
+    If ((Get-Content $Profile) -Ne $Dotfile) {
+        Write-Host "Same"
+    }
+}
+
+Function .. {
+    Set-Location ..
+}
+Function ... {
+    Set-Location ../..
+}
+Function .... {
+    Set-Location ../../..
+}
+Function ..... {
+    Set-Location ../../../..
+}
+Function ...... {
+    Set-Location ../../../../..
+}
+
+Function Scripts {
+    Set-Location ~/Projects
+}
+
+Function Temp {
+    Set-Location C:\Temp
+}
+
+# Aliases
+Set-Alias -Name Q -Value Ex
+Set-Alias -Name C -Value "Clear"
+Set-Alias -Name Ip -Value "Get-Ips"
+Set-Alias -Name Dotfiles -Value Update-Dotfiles
+Set-Alias -Name tmep -Value Temp
+Set-Alias -Name system -Value Start-PowershellSystem
+
+
+#Run Functions
+Import-Module -Name Psreadline
+Set-Psreadlineoption -Historynoduplicates
+Set-Psreadlinekeyhandler -Key Tab -Function Menucomplete
+Set-Psreadlineoption -Predictionsource History
+#Set-Psreadlinekeyhandler -Key Uparrow -Function Historysearchbackward
+#Set-Psreadlinekeyhandler -Key Downarrow -Function Historysearchforward
+Clear
+
+
+Import-Module -Name PSReadline
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -PredictionSource History
+#Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+#Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+clear
+Invoke-Expression (&starship init powershell)
+function Start-PowershellSystem {
+C:\Software\PSTools\PsExec64.exe -s -i powershell.exe -accepteula
+}
+function Start-PowershellSystem {
+C:\Software\PSTools\PsExec64.exe -s -i powershell.exe -accepteula
+}
